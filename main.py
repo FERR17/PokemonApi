6d463adb2beadec2d193b9457e11a000670bb387
FERR17 <134084732+FERR17@users.noreply.github.com>
2023-05-29 12:39:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,5 +17,4 @@ def conversorCaF(C):
          C=float(C)
          TF=C*(9/5) + 32
          return f"La temperatura es de {TF} grados Farenheit"
-    except:
-    return "Entrada invalida"
+       

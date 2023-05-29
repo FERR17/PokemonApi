85eb2e9ad47758e77392b93bcc11e0c7c942aa74
FERR17 <134084732+FERR17@users.noreply.github.com>
2023-05-29 14:15:22 -0600

    main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ app=FastAPI()
 
 @app.get("/")
 def index():
-    return "Hola a todos, quieres saber de pokemon"
+     return "Hola a todos, quieres saber de pokemon"
 
 @app.get("/Pokemon/{num}")
 def pokemon(num):
-    pokemons={
-    "1":"Bulbasaur",
-    "2":"Ivysaur",
-    "3":"Venasaur",
-    "4":"Charmander"
+        pokemons={
+         "1":"Bulbasaur",
+         "2":"Ivysaur",
+         "3":"Venasaur",
+         "4":"Charmander"
      
     }
     return pokemons{num}
@@ -20,8 +20,8 @@ def pokemon(num):
 @app.get("/Conversor_CaF/{C}")
 def conversorCaF(C):
     try:
-        C=float(C)
-        TF=C*(9/5) + 32
-        return f"La temperatura es de {TF} grados Farenheit"
+            C=float(C)
+            TF=C*(9/5) + 32
+            return f"La temperatura es de {TF} grados Farenheit"
     except:
-         return "Entrada invalida"
+            return "Entrada invalida"

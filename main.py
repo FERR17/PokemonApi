35c7d98810574c150fef82836d021517678afab7
FERR17 <134084732+FERR17@users.noreply.github.com>
2023-05-29 12:35:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def pokemon(num):
 
 @app.get("/Conversor_CaF/{C}")
 def conversorCaF(C):
-try:
-    C=float(C)
-    TF=C*(9/5) + 32
-    return f"La temperatura es de {TF} grados Farenheit"
-except:
+    try:
+         C=float(C)
+         TF=C*(9/5) + 32
+         return f"La temperatura es de {TF} grados Farenheit"
+    except:
     return "Entrada invalida"

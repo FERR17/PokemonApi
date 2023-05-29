8c85bfdf03ceffb570ebcd64ad13e389c257fc54
FERR17 <134084732+FERR17@users.noreply.github.com>
2023-05-29 13:44:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ def index():
 def pokemon(num):
     pokemons={
     "1":"Bulbasaur",
-        "2":"Ivysaur",
-            "3":"Venasaur"
-                "4":"Charmander",
+    "2":"Ivysaur",
+    "3":"Venasaur"
+    "4":"Charmander",
         }
     return pokemons{num}
 
 @app.get("/Conversor_CaF/{C}")
 def conversorCaF(C):
-try:
-    C=float(C)
-    TF=C*(9/5) + 32
-    return f"La temperatura es de {TF} grados Farenheit"
-except:
+    try:
+        C=float(C)
+        TF=C*(9/5) + 32
+        return f"La temperatura es de {TF} grados Farenheit"
+    except:
     return "Entrada invalida"
